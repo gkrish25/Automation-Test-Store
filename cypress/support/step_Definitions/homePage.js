1c3745534a18aa8b3a31ce68b3a60a261d927fe4
Gokul Krishna <gokulkrishna433@gmail.com>
2025-03-12 22:20:14 +0530

    Formatted step definition files

--- cypress/support/step_Definitions/homePage.js
+++ cypress/support/step_Definitions/homePage.js
@@ -1,24 +1,33 @@
-const { Given, When, Then } = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  Given,
+  When,
+  Then,
+} = require("@badeball/cypress-cucumber-preprocessor");
 
-Then('I navigate to Home page', () => {
-    cy.get('.menu_home').click();
-    cy.get('.banner_container').should('be.visible');
-})
+Then("I navigate to Home page", () => {
+  cy.get(".menu_home").click();
+  cy.get(".banner_container").should("be.visible");
+});
 
-When ("I MouseOver on the first {string} and click {string}", (product,category)=>{
-    cy.get('#categorymenu').within(()=>{
-        cy.get('.categorymenu').find('a').contains(category).trigger('mouseover');
-        cy.get('.subcategories').find('a').contains(product).click({force:true});
-        cy.get('.heading1').contains(product);
-    })
-    
-})
+When(
+  "I MouseOver on the first {string} and click {string}",
+  (product, category) => {
+    cy.get("#categorymenu").within(() => {
+      cy.get(".categorymenu").find("a").contains(category).trigger("mouseover");
+      cy.get(".subcategories")
+        .find("a")
+        .contains(product)
+        .click({ force: true });
+      cy.get(".heading1").contains(product);
+    });
+  }
+);
 
-When ("I select any product {string}",(productName)=>{
-    cy.get('.list-inline').find('.prdocutname').contains(productName).click();
-    cy.get('.productname').find('.bgnone').contains(productName);
-})
+When("I select any product {string}", (productName) => {
+  cy.get(".list-inline").find(".prdocutname").contains(productName).click();
+  cy.get(".productname").find(".bgnone").contains(productName);
+});
 
-Then("I verify the Total on cart screen",()=>{
-    //cy.get('.table-striped').find('td')
-})
+Then("I verify the Total on cart screen", () => {
+  //cy.get('.table-striped').find('td')
+});
--- cypress/support/step_Definitions/loginpage.js
+++ cypress/support/step_Definitions/loginpage.js
@@ -1,31 +1,32 @@
-const { Given, When, Then } = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  Given,
+  When,
+  Then,
+} = require("@badeball/cypress-cucumber-preprocessor");
 
+Given("I open Automation Test store", () => {
+  cy.visit("https://automationteststore.com/");
+  cy.url().should("include", "automationteststore");
+});
 
-Given('I open Automation Test store', () => {
-    cy.visit("https://automationteststore.com/")
-    cy.url().should("include", "automationteststore")
-})
+When("I click on Account button", () => {
+  cy.get("#customer_menu_top").click();
+  cy.get("h1").should("contains.text", "Account Login");
+});
 
-When("I click on Account button", ()=>{
-    cy.get('#customer_menu_top').click();
-    cy.get('h1').should('contains.text', 'Account Login')
-    
-})
+Then("I enter the username and Password", () => {
+  cy.get("#loginFrm_loginname").type("Gnallasw");
+  cy.get("#loginFrm_password").type("Gokulash@25");
+  cy.get("button").contains("Login").click();
+});
 
-Then("I enter the username and Password",()=>{
-    cy.get('#loginFrm_loginname').type("Gnallasw");
-    cy.get('#loginFrm_password').type('Gokulash@25');
-    cy.get('button').contains('Login').click();
-})
+Given("I lauch Automation test store", () => {
+  cy.logintoAutomationStore("Gnallasw", "Gokulash@25");
+});
 
-Given('I lauch Automation test store', () => {
-    cy.logintoAutomationStore("Gnallasw", "Gokulash@25");
-})
-
-When ("I MouseOver on the {string} and click {string}", (category,product)=>{
-    cy.get('#categorymenu').within(()=>{
-        cy.get('.categorymenu').find('a').contains(category).trigger('mouseover');
-        cy.get('.subcategories').find('a').contains(product).click({force:true});
-    })
-    
-})
+When("I MouseOver on the {string} and click {string}", (category, product) => {
+  cy.get("#categorymenu").within(() => {
+    cy.get(".categorymenu").find("a").contains(category).trigger("mouseover");
+    cy.get(".subcategories").find("a").contains(product).click({ force: true });
+  });
+});
