2972a79aebede01b7d0b3dfb85b47bda3493170d
Gokul Krishna <gokulkrishna433@gmail.com>
2025-03-12 22:14:18 +0530

    Added new step definition file - productPage

--- cypress/support/step_Definitions/homePage.js
+++ cypress/support/step_Definitions/homePage.js
@@ -19,42 +19,6 @@ When ("I select any product {string}",(productName)=>{
     cy.get('.productname').find('.bgnone').contains(productName);
 })
 
-Then("I update the quantity {string}",(quantity)=>{
-    cy.get('#product_quantity').clear().type(quantity)
-})
-
-When("I click on Add to Cart",()=>{
-    cy.get('.productpagecart').click();
-    cy.url().should('include','checkout/cart');
-    cy.get('.heading1').contains('Shopping Cart')
-})
-
-When("I click on Add to wishlist",()=>{
-    cy.get('.wishlist').find('.wishlist_add').click();
-    cy.get('.wishlist_remove').should('be.visible');
-})
-
-Then("I verify the total price",()=>{
-    cy.get('#product_details').within(()=>{
-        let productPrice;
-      cy.get('.productprice').find('.productfilneprice').then((pPrice)=>{
-        productPrice = pPrice.text();
-        cy.log(productPrice)
-      });
-      
-      let quantity 
-      cy.get('.quantitybox').find('#product_quantity').then((quan)=>{
-        quantity = quan.val();
-        cy.log(quantity)
-      })
-      
-      cy.get('.total-price').then((price)=>{
-        let totalPrice = price.text();
-        cy.log(totalPrice)
-      })
-    })
-})
-
 Then("I verify the Total on cart screen",()=>{
     //cy.get('.table-striped').find('td')
 })
